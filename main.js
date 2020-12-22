ce11549b842255164baf3bf4a3237cef2ec2a05d
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-22 20:37:30 +0800

    add more styling

--- main.js
+++ main.js
@@ -78,6 +78,22 @@ $('document').ready(() => {
         $(this).addClass('active').siblings().removeClass('active');
     })
 
+    // back to top button
+    let btn = $('#button');
+
+    $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+    });
+
+    btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+    });
+
 })
 
 
